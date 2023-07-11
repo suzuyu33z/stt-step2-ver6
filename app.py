d3692ab05626a93c6764bc6e34ec70d1b655af9e
ryuki.hamada <54260904+hbsryuryu@users.noreply.github.com>
2023-07-11 13:37:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 
 import streamlit as st # フロントエンドを扱うstreamlitの機能をインポート
 import speech_recognition as sr # 音声認識の機能をインポート
+from bokeh.models.widgets import Button
+from bokeh.models import ColumnDataSource,CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
 
 # 言語選択と、APIが認識する言語の変換リストを作成
 set_language_list = {
@@ -45,3 +48,58 @@ if (file_upload !=None):
 
 
 st.write("マイクでの音声認識はこちらのボタンから") # 案内表示
+
+
+stt_button = Button(label="音声認識開始",width=123,height=36,margin=0)
+
+
+if (set_language_list[set_language] == "ja"):
+    stt_button.js_on_event("button_click", CustomJS(code="""
+                                                    
+        
+
+        //音声認識APIの使用
+        var speech = new webkitSpeechRecognition();
+        speech.lang = "ja";
+        speech.start();                                  
+                                                    
+        speech.addEventListener("result", function (e) {
+            // 音声認識で取得した情報を表示
+            var text = e.results[0][0].transcript;
+            document.dispatchEvent(new CustomEvent("test", {detail: {return_text: text}}))
+        });
+                                                
+
+        """))
+else:
+    stt_button.js_on_event("button_click", CustomJS(code="""
+
+        //音声認識APIの使用
+        var speech = new webkitSpeechRecognition();
+        speech.lang = "en-US";
+        speech.start();                                  
+                                                    
+        speech.addEventListener("result", function (e) {
+            // 音声認識で取得した情報を表示
+            var text = e.results[0][0].transcript;
+            document.dispatchEvent(new CustomEvent("test", {detail: {return_text: text}}))
+        });
+                                                
+
+        """))
+
+#keysは任意のid
+# document.dispatchEventで変数を受け取る
+result2 = streamlit_bokeh_events(
+    stt_button,
+    events="test",
+    key="stt",
+    refresh_on_update=False,
+    override_height=45,
+    debounce_time=0)
+state = st.empty() # マイク録音中を示す為の箱を準備
+if (result2 != None):
+    print(result2["test"]["return_text"])
+    state.write("音声認識結果:")
+    st.write(result2["test"]["return_text"])
+
